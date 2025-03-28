a40830c3174cc39543826760f7394a2b1ff54378
Shannon M. Hauck <shannonmariehauck@gmail.com>
2025-03-28 18:10:42 -0400

    allow unauthenticated access to static pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
+  allow_unauthenticated_access only: %i[ about faq index privacy roadmap show terms ]
   before_action :resume_session
   before_action :set_page, only: %i[ edit update destroy ]
-  allow_unauthenticated_access only: %i[ index show ]
 
 
   def about
