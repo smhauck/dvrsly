75cb12117c3adf1d59e601b7a8e235b707810199
Shannon M. Hauck <shannonmariehauck@gmail.com>
2025-04-27 21:14:55 -0400

    only show 250 chars in posts/index

--- script/changelog_generator.rb
+++ script/changelog_generator.rb
@@ -1,7 +1,9 @@
+#!ruby
+
 # Recreate the Changelog page
 
 # changelog_file = Rails.root.to_s + "/app/views/pages/changelog.html.erb"
-changelog_file = Rails.root.join("/app/views/pages/changelog.html.erb")
+changelog_file = "../app/views/pages/changelog.html.erb"
 
 f = File.open(changelog_file, "w")
 f.write("<h1>Changelog</h1>")
